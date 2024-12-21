4535385508f3e9c6b8e7aa0e6e150d440c225d00
ntbinh06 <binhqb2004@gmail.com>
2024-12-21 11:04:04 +0700

    first commit

--- app/src/main/java/com/ktct124/lop124LTDD04/nhom14/MenberFragment.java
+++ app/src/main/java/com/ktct124/lop124LTDD04/nhom14/MenberFragment.java
@@ -7,58 +7,61 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import androidx.annotation.NonNull;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MenberFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MenberFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MenberFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MenberFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MenberFragment newInstance(String param1, String param2) {
-        MenberFragment fragment = new MenberFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private TextView tvMember1, tvMember2, tvMember3;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_menber, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menber, container, false);
+        // Ánh xạ các TextView
+        tvMember1 = view.findViewById(R.id.member1);
+        tvMember2 = view.findViewById(R.id.member2);
+        tvMember3 = view.findViewById(R.id.member3);
+
+        // Kết nối đến Firebase Realtime Database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference membersRef = database.getReference("members");
+
+        // Đọc dữ liệu từ Firebase
+        membersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    int index = 1;
+                    for (DataSnapshot child : snapshot.getChildren()) {
+                        String name = child.child("name").getValue(String.class);
+                        String msv = child.child("msv").getValue(String.class);
+
+                        String displayText = name + ". MSV: " + msv;
+                        if (index == 1) {
+                            tvMember1.setText(displayText);
+                        } else if (index == 2) {
+                            tvMember2.setText(displayText);
+                        } else if (index == 3) {
+                            tvMember3.setText(displayText);
+                        }
+                        index++;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Xử lý lỗi
+                tvMember1.setText("Failed to load data");
+            }
+        });
+
+        return view;
     }
 }
